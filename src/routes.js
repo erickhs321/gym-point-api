35f61b618241d38501c624996146f66cdd009137
Erick <erickhenrique321@gmail.com>
2020-02-18 20:30:12 -0300

    session controller

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) =>
-  res.json({
-    message: 'hello world',
-  })
-);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
