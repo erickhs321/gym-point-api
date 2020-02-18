c28b36cdbb5259afb9629998dc166a49a87a623a
Erick <erickhenrique321@gmail.com>
2020-02-18 20:13:11 -0300

    user model, auth config, models map

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+import authConfig from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {}
+}
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'a6e035521fe635b42dca6d2272e71819',
+  expiresIn: '7d',
+};
